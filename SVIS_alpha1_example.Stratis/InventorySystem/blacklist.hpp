010cc970bc9673c45d01457ab202deb41a83e348
sbobovyc <stan.bobovych@gmail.com>
2013-03-21 00:35:08 -0400

    Added blacklist functionality to weapons, items and backpacks

---
+++ SVIS_alpha1_example.Stratis/InventorySystem/blacklist.hpp
@@ -0,0 +1,41 @@
+/**
+  * @file blacklist.hpp
+  * @author sbobovyc
+  * @details
+  * Use this file to blacklist weapons, backpacks, and items. You can only use one blacklist for each category at a time. A blacklist for each category has to exist, even if it's
+  * a blank one.
+  */
+
+// This is an empty weapons blacklist
+#define WEAPON_BLACKLIST []
+
+// This blacklists all Khaybar weapons
+/*
+#define WEAPON_BLACKLIST \
+[ \
+	"arifle_Khaybar_F",\
+	"arifle_Khaybar_C_F",\
+	"arifle_Khaybar_GL_F",\
+	"arifle_Khaybar_C_Nstalker_point_mzls_F",\
+	"arifle_Khaybar_C_Holo_F",\
+	"arifle_Khaybar_C_Holo_mzls_F",\
+	"arifle_Khaybar_C_Holo_point_F",\
+	"arifle_Khaybar_C_ACO_point_F",\
+	"arifle_Khaybar_C_ACOg_point_F",\
+	"arifle_Khaybar_C_ACO_flash_mzls_F",\
+	"arifle_Khaybar_C_ACOg_flash_mzls_F",\
+	"arifle_Khaybar_C_ACO_F",\
+	"arifle_Khaybar_C_ACOg_F",\
+	"arifle_Khaybar_ACO_point_F",\
+	"arifle_Khaybar_ACOg_point_F",\
+	"arifle_Khaybar_ARCO_point_F",\
+	"arifle_Khaybar_Holo_mzls_F",\
+	"arifle_Khaybar_GL_ACO_point_F",\
+	"arifle_Khaybar_GL_ACOg_point_F",\
+	"arifle_Khaybar_GL_ACO_point_mzls_F",\
+	"arifle_Khaybar_GL_ACOg_point_mzls_F"\
+]
+*/
+
+#define ITEM_BLACKLIST []
+#define BACKPACK_BLACKLIST []

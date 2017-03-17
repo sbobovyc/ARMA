df561401987a7bd16ecf5948c47a8499cd1181bf
Stan Bobovych <stan.bobovych@gmail.com>
2017-03-17 19:06:09 -0400

    Added workshop item downloads to arma 3 steamcmd update script

--- server/update_arma3.sh
+++ server/update_arma3.sh
@@ -1,3 +1,23 @@
-USER=steyermaster
-PASS=$1
-./steamcmd.sh +login $USER $PASS +app_update 233780
+USER=$1
+PASS=$2
+ARMA_ID=107410
+ARMA_SERVER_ID=233780
+RHSUSAF_ID=843577117
+RHSAFRF_ID=843425103
+RHSGREF_ID=843593391
+INSTALL_DIR=/home/redguard/Arma3
+WORKSHOP_DIR=/home/redguard/steamcmd/steamapps/workshop/content
+
+download_mod() {
+	MODID=$1
+	./steamcmd.sh +login $USER $PASS +force_install_dir $INSTALL_DIR +workshop_download_item $ARMA_ID $MODID validate +quit
+}
+
+./steamcmd.sh +login $USER $PASS +force_install_dir $INSTALL_DIR +app_update $ARMA_SERVER_ID +quit
+download_mod $RHSUSAF_ID
+download_mod $RHSAFRF_ID
+download_mod $RHSGREF_ID
+
+ln -s $WORKSHOP_DIR/$ARMA_ID/$RHSUSAF_ID $INSTALL_DIR/@rhsusaf
+ln -s $WORKSHOP_DIR/$ARMA_ID/$RHSAFRF_ID $INSTALL_DIR/@rhsafrf
+ln -s $WORKSHOP_DIR/$ARMA_ID/$RHSGREF_ID $INSTALL_DIR/@rhsgref

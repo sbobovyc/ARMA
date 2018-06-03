21562368a62f97301f6e13fe3f26ccfc3cf786e8
Stan Bobovych <stan.bobovych@gmail.com>
2018-06-03 15:28:56 -0400

    Added CBA

--- server/update_arma3.sh
+++ server/update_arma3.sh
@@ -5,19 +5,22 @@ ARMA_SERVER_ID=233780
 RHSUSAF_ID=843577117
 RHSAFRF_ID=843425103
 RHSGREF_ID=843593391
-INSTALL_DIR=/home/redguard/Arma3
-WORKSHOP_DIR=/home/redguard/steamcmd/steamapps/workshop/content
+CBA3_ID=450814997
+INSTALL_DIR=/home/sbobovyc/Arma3
+WORKSHOP_DIR=/home/sbobovyc/Arma3/steamapps/workshop/content
 
 download_mod() {
 	MODID=$1
-	./steamcmd.sh +login $USER $PASS +force_install_dir $INSTALL_DIR +workshop_download_item $ARMA_ID $MODID validate +quit
+	steamcmd +login $USER $PASS +force_install_dir $INSTALL_DIR +workshop_download_item $ARMA_ID $MODID validate +quit
 }
 
-./steamcmd.sh +login $USER $PASS +force_install_dir $INSTALL_DIR +app_update $ARMA_SERVER_ID +quit
+steamcmd +login $USER $PASS +force_install_dir $INSTALL_DIR +app_update $ARMA_SERVER_ID +quit
 download_mod $RHSUSAF_ID
 download_mod $RHSAFRF_ID
 download_mod $RHSGREF_ID
+download_mod $CBA3_ID
 
 ln -s -T $WORKSHOP_DIR/$ARMA_ID/$RHSUSAF_ID $INSTALL_DIR/@rhsusaf
 ln -s -T $WORKSHOP_DIR/$ARMA_ID/$RHSAFRF_ID $INSTALL_DIR/@rhsafrf
 ln -s -T $WORKSHOP_DIR/$ARMA_ID/$RHSGREF_ID $INSTALL_DIR/@rhsgref
+ln -s -T $WORKSHOP_DIR/$ARMA_ID/$CBA3_ID $INSTALL_DIR/@cba_a3
